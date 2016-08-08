2f4cdc5e2d2a2678b8d56364f2b61d013c89de40
sourav Mondal <souravm@capitalnumbers.com>
2016-08-08 15:25:46 +0530

    added the routes for application

--- routes.php
+++ routes.php
@@ -1,3 +1,27 @@
 <?php
 
+use Controllers\UserController;
+use Utilities\ApiAuthencation;
 
+$app['UserController.controller'] = $app->factory(function() use ($app) {
+    return new UserController($app);
+});
+
+$app['ApiAuthencation'] = $app->factory(function() use ($app) {
+    return new ApiAuthencation($app);
+});
+
+$app->before('ApiAuthencation:authenticate');
+
+$app->get('/', function () use ($app) {
+    return new Symfony\Component\HttpFoundation\JsonResponse(array(
+        'status' => 'api is operating normally',
+        'version' => '1.0.0'
+    ), 200);
+});
+$app->get('/users', 'UserController.controller:getAll');
+$app->get('/user/{userId}', 'UserController.controller:getUser');
+$app->post('/user/add', 'UserController.controller:addUser');
+$app->put('/user/update/{userId}', 'UserController.controller:updateUser');
+$app->delete('/user/delete/{userId}', 'UserController.controller:deleteUser');
+$app->post('/user/authencate', 'UserController.controller:login');
